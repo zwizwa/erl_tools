cf4ccee776dd4b3cd17034368cf105234e182b6a
Tom Schouten <tom@zwizwa.be>
2016-03-23 16:49:44 -0400

    p_rem, p_div

--- src/tools.erl
+++ src/tools.erl
@@ -4,7 +4,7 @@
          strunk/1, getter/1, 
          format/2, creader/1, int/1, float/1, hex_data/1, enumerate/1, chunks/2, nchunks/3,
          unpack/2, unpack_s32/1, unpack_u16/1, unpack_s16/1,
-         mod/2, divide/2, round_up/2,
+         p_rem/2, p_div/2, round_up/2,
          mid/2, mid/3,
          csv_read/1,
          padded_at/2, padded_range/3,
@@ -176,18 +176,18 @@ unpack_s16(L) ->
 
 
 %% Modulo with positive remainder.
-mod(X,Y) when X > 0 -> X rem Y;
-mod(X,Y) when X < 0 -> Y + X rem Y;
-mod(0,_) -> 0.
+p_rem(X,Y) when X > 0 -> X rem Y;
+p_rem(X,Y) when X < 0 -> Y + X rem Y;
+p_rem(0,_) -> 0.
 
-%% Div with positive remainder.
-divide(X,Y) -> (X - mod(X,Y)) div Y.
+%% Divide with positive remainder.
+p_div(X,Y) -> (X - p_rem(X,Y)) div Y.
 
     
 
 %% Round up to next multiple
 round_up(El,Chunk) ->
-    (divide(El-1,Chunk)+1)*Chunk.
+    (p_div(El-1,Chunk)+1)*Chunk.
 
 %% Midpoint, picking smallest if neighbours.
 mid(A,B) -> (A + B) div 2.
