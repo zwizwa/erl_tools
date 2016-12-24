22f0b4c2d62c18d69678a6558336e82f13159cbf
Tom Schouten <tom@zwizwa.be>
2016-12-24 14:46:11 -0500

    iseq

--- src/iseq.erl
+++ src/iseq.erl
@@ -1,27 +1,34 @@
 -module(iseq).
--export([cycle/1, take/2, periodic/2]).
+-export([cycle/1, take/2, periodic/2, map/2]).
 
 %% iseq : infinite sequences represent as thunks
 %% Evaluating the thunk produces an {element, iseq} pair.
 
-cycle({_,Init}=Spec, []) ->
-    cycle(Spec, Init);
+cycle(List, []) ->
+    cycle(List, List);
 cycle(Spec, [Head|Tail]) ->
     {Head, fun() -> cycle(Spec, Tail) end}.
-cycle(OrdSet=[_|_]) ->
-    fun() -> cycle(OrdSet, OrdSet) end.
+cycle(List=[_|_]) ->
+    fun() -> cycle(List, List) end.
 
 
-periodic({_,Init}=Spec, Loop, []) ->
-    periodic(Spec, Loop+1, Init);
+periodic({_,List}=Spec, Loop, []) ->
+    periodic(Spec, Loop+1, List);
 periodic({Period, _} = Spec, Loop, [Head|Tail]) ->
     {Loop * Period + Head, 
      fun() -> periodic(Spec, Loop, Tail) end}.
-periodic(Period, OrdSet=[_|_]) ->
-    fun() -> periodic({Period, OrdSet}, 0, OrdSet) end.
+periodic(Period, List=[_|_]) ->
+    fun() -> periodic({Period, List}, 0, List) end.
 
 
 take(_,0) -> [];
-take(Cycle,N) when N > 1 -> 
-    {E,C} = Cycle(),
+take(Seq,N) when N > 0 -> 
+    {E,C} = Seq(),
     [E|take(C, N-1)].
+
+
+map(Fun, Seq) ->
+    fun() -> {Head, Tail} = Seq(), {Fun(Head), map(Fun, Tail)} end.
+
+             
+            
