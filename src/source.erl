6c791996f22f022a0aa66a41d17d212282463c59
Tom Schouten <tom@zwizwa.be>
2017-01-18 17:39:17 +0100

    source:to_fold

--- src/source.erl
+++ src/source.erl
@@ -1,5 +1,5 @@
 -module(source).
--export([range/2,range/1,to_list/1]).
+-export([range/2,range/1,to_list/1,to_fold/1]).
 
 %% Internal iterators -- dual of sink, represented as pairs with
 %% delayed tails.
@@ -13,5 +13,13 @@ to_list(Source) ->
         {E,S} -> [E | to_list(S)];
         eof -> []
     end.
-            
 
+to_fold(Source) ->
+    fun(F,I) -> to_fold(Source, F, I) end.
+to_fold(Source, F, S) ->
+    case Source() of
+        {E, Src} -> to_fold(Src, F, F(E, S));
+        eof -> S
+    end.
+            
+            
