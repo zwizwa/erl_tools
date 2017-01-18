fc20aa56257c1e2da67d5efd3efb07ae0f57d8e6
Tom Schouten <tom@zwizwa.be>
2017-01-18 17:58:50 +0100

    source:map

--- src/source.erl
+++ src/source.erl
@@ -1,5 +1,5 @@
 -module(source).
--export([range/2,range/1,to_list/1,to_fold/1]).
+-export([range/2,range/1,to_list/1,to_fold/1,map/2]).
 
 %% Internal iterators -- dual of sink, represented as pairs with
 %% delayed tails.
@@ -8,18 +8,27 @@ range(N) -> range(0,N).
 range(E,N) when E < N -> fun() -> {E, range(E+1,N)} end;
 range(_,_) -> fun() -> eof end.
 
-to_list(Source) ->
-    case Source() of
-        {E,S} -> [E | to_list(S)];
+to_list(Src) ->
+    case Src() of
+        {El, NextSrc} -> [El | to_list(NextSrc)];
         eof -> []
     end.
 
-to_fold(Source) ->
-    fun(F,I) -> to_fold(Source, F, I) end.
-to_fold(Source, F, S) ->
-    case Source() of
-        {E, Src} -> to_fold(Src, F, F(E, S));
+to_fold(Src) ->
+    fun(F,I) -> to_fold(Src, F, I) end.
+to_fold(Src, F, S) ->
+    case Src() of
+        {E, NextSrc} -> to_fold(NextSrc, F, F(E, S));
         eof -> S
+    end.
+            
+
+map(F,Src) ->
+    fun() ->
+            case Src() of
+                {El, NextSrc} -> {F(El), map(F,NextSrc)};
+                eof -> eof
+            end
     end.
             
             
