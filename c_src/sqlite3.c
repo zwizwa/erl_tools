cd9d2b58e202dc9d953bc2448de62be877b00ada
Tom Schouten <tom@zwizwa.be>
2017-07-03 14:10:15 -0400

    sqlite3

---
+++ c_src/sqlite3.c
@@ -0,0 +1,234 @@
+#include "system.h"
+#include <stdio.h>
+#include <sqlite3.h>
+
+#include "bert.h"
+
+// https://www.sqlite.org/c_interface.html
+// https://www.sqlite.org/quickstart.html
+
+sqlite3 *db;
+
+/* Use the generalized right fold over the erlang term format provided
+   in bert.[ch] to iterate over the data primitives in the order they
+   appear in the stream.
+
+   Ignore tuple and list structure, and primitive atoms except for
+   atom and binary, effectively flattening and filtering the input
+   into a sequence of binaries and atoms:
+
+   E.g. {<<"insert into config (var,val) values (?,?)">>,
+         [{text,<<"myvar">>},
+          {blob,<<"myval">>}]}
+
+   Flattens to these constructor calls in sequence:
+
+     binary(<query string>)
+     atom('text'),
+     binary(<<"myvar">>)
+     atom('blob')
+     binary(<<"myval">>)
+
+   The meaning of an atom is then to set the sqlite3 binding type of
+   the subsequent binary.  After that, the binding type is restored to
+   text.  Binaries are collected in a arg list.
+
+   The first binary is the query string.  The remaining binaries are
+   arg bindings. */
+
+typedef int (*bind_t)(sqlite3_stmt*, int, const void*, int n, void(*)(void*));
+struct slice {
+    bind_t bind;
+    uint8_t *buf;
+    uint32_t len;
+};
+#define MAX_NB_SLICES 10
+struct reader {
+    struct bert_reader r;
+    uint8_t *buf;
+    uint32_t offset, length, nb_slices;
+    bind_t bind;
+    struct slice arg[MAX_NB_SLICES];
+};
+// dummy type - values are not used, only traversal side effect
+struct bert_object {};
+static void wind(struct reader *r, uint32_t nb_bytes) {
+    uint32_t new_offset = r->offset + nb_bytes;
+    if (new_offset > r->length) { ERROR("message truncated"); }
+    r->offset = new_offset;
+}
+static uint8_t pop(struct reader *r) {
+    uint8_t byte = r->buf[r->offset];
+    wind(r, 1);
+    return byte;
+}
+// An atom in the stream sets the current binder, which is picked up by binary().
+static int blobtype(struct reader *r, uint32_t nb_bytes, const char *cstring) {
+    return (strlen(cstring) == nb_bytes)
+        && (!strncmp(cstring, r->buf+r->offset, nb_bytes));
+}
+static struct bert_object *atom(struct reader *r, uint32_t nb_bytes) {
+    if (blobtype(r,nb_bytes,"blob")) {r->bind = sqlite3_bind_blob; return NULL;}
+    if (blobtype(r,nb_bytes,"text")) {r->bind = sqlite3_bind_text; return NULL;}
+    ERROR("unknown bind type");
+}
+// Side effect stores slice in current slot.
+static struct bert_object* binary(struct reader *r, uint32_t nb_bytes) {
+    if (r->nb_slices >= MAX_NB_SLICES) { ERROR("MAX_NB_SLICES"); }
+    struct slice *s = &r->arg[r->nb_slices++];
+    s->bind = r->bind;
+    s->buf = r->buf + r->offset;
+    s->len = nb_bytes;
+    r->bind = sqlite3_bind_text; // restore default
+    return NULL;
+}
+
+
+static void parse_error(struct reader *r, const char *msg) {
+    ERROR(msg);
+}
+
+/* Generic send */
+void msg_send(struct bert_writer *w,
+              uint8_t *buf, uint32_t len) {
+    //LOG("msg_send(%p, %d)\n", buf, len);
+    assert_write(1, buf, len);
+}
+
+
+/* Package error message and send it to Erlang side. */
+struct sql_error {
+    struct bert_writer w;
+    const char *msg;
+};
+void sql_error_seq(struct bert_writer *w) {
+    struct sql_error *err = (void*)w;
+    w->small_tuple(w, 2);
+    w->atom  (w, "error", -1);
+    w->binary(w, err->msg, -1);
+}
+void sql_error(int rv) {
+    struct sql_error err = {
+        .msg = sqlite3_errmsg(db),
+    };
+    bert_write_packet(&err.w, 4, &sql_error_seq, &msg_send);
+
+    // Also send on console and abort.
+    ERROR("sql_error(%s)", err.msg);
+}
+
+
+struct sql_row {
+    struct bert_writer w;
+    struct slice *col;
+    uint32_t nb_cols;
+};
+void sql_row_seq(struct bert_writer *w) {
+    struct sql_row *row = (void*)w;
+    w->list(w, row->nb_cols);
+    for(uint32_t c=0; c<row->nb_cols; c++){
+        struct slice *s = &row->col[c];
+        w->binary(w, s->buf, s->len);
+    }
+    w->nil(w);
+}
+
+void query(const uint8_t *buf, uint32_t length) {
+
+    /* Convert BERT to list of binary terms. */
+    struct reader r = {
+        .buf = buf,
+        .length = length,
+        .bind = sqlite3_bind_text,
+        .r = {
+            .pop    = (void*)&pop,
+            .wind   = (void*)&wind,
+            .error  = (void*)&parse_error,
+            /* We expect [binary() | [{atom(), binary()}]] */
+            .binary = (void*)&binary,
+            .atom   = (void*)&atom,
+        }
+    };
+    bert_decode(&r); // run for side-effect
+    if (r.nb_slices == 0) { ERROR("empty statement"); }
+
+
+    /* First term is the query string. */
+    sqlite3_stmt *stmt;
+    int rv = sqlite3_prepare_v2(db, r.arg[0].buf, r.arg[0].len, &stmt, NULL);
+    if (rv != SQLITE_OK) { sql_error(rv); }
+
+    /* Subsequent terms are optional blob values to bind to the
+       statement. */
+    for(uint32_t i=1; i<r.nb_slices; i++) {
+        struct slice *a = &r.arg[i];
+        //LOG("bind %d %d\n", i, s->len);
+        a->bind(stmt, i, a->buf, a->len, SQLITE_STATIC /*ok?*/);
+    }
+
+    /* Step through the query results, sending out rows as list of
+       binaries. */
+    while(1) {
+        int rv = sqlite3_step(stmt);
+        if (rv == SQLITE_ROW) {
+
+            /* Get rows converted to binary */
+            int nb_cols = sqlite3_column_count(stmt);
+            if (!nb_cols) break;
+            struct slice col[nb_cols];
+            for (int c=0; c<nb_cols; c++) {
+                col[c].buf = sqlite3_column_blob(stmt, c);
+                col[c].len = sqlite3_column_bytes(stmt, c);
+            }
+
+            /* Send it out as a list of binaries. */
+            struct sql_row row = {
+                .nb_cols = nb_cols,
+                .col = col
+            };
+            bert_write_packet(&row.w, 4, &sql_row_seq, &msg_send);
+        }
+        else if (rv == SQLITE_DONE) {
+            break;
+        }
+        else {
+            sql_error(rv);
+        }
+    }
+    sqlite3_finalize(stmt);
+    /* Terminate with an empty message */
+    uint8_t sentinel[4] = {};
+    assert_write(1, sentinel, sizeof(sentinel));
+    fflush(stdout); // ?
+}
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        LOG("%s (build " BUILD ")\n", argv[0]);
+        LOG("usage: %s <dbfile>\n", argv[0]);
+        exit(1);
+    }
+    int rc;
+    //erl_init(NULL,0);
+    if ((rc = sqlite3_open(argv[1], &db))) {
+        LOG("%s: %s\n", argv[1], sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+    void *q;
+
+    uint8_t *msg;
+    uint32_t msg_len;
+    // LOG("entering message loop\n");
+    while((msg_len = assert_read_u32(0))) {
+        if (!(msg = malloc(msg_len))) {
+            ERROR("malloc(0x%08x) failed\n", msg_len);
+        }
+        assert_read_fixed(0, msg, msg_len);
+        query(msg,msg_len);
+        free(msg);
+    }
+    sqlite3_close(db);
+    return 0;
+}
+
---
+++ src/sqlite3.erl
@@ -0,0 +1,120 @@
+-module(sqlite3).
+-export([open/1, close/1, query/3,
+         select/5, select_m/5, select/4,
+         delete/4,
+         insert_or_replace/5,
+         insert_or_replace_m/3
+        ]).
+
+%% Port operations.
+open(DbFile) ->
+    Cmd = tools:format("priv/sqlite3.elf ~s", [DbFile]),
+    open_port({spawn, Cmd}, [use_stdio, {packet,4}, exit_status, binary]).
+close(Port) ->
+    Port ! {self(), {command, <<>>}},
+    close_flush(Port).
+close_flush(Port) ->
+    receive
+        {Port, {exit_status, 0}} -> ok;
+        {Port, {exit_status, _}=E} -> exit(E);
+        {Port, _} = M -> 
+            log:info("close_flush ~p~n",[M]),
+            close_flush(Port)
+    end.
+query(Port, {SQL, Bindings}=Query, Sink) when is_binary(SQL) and is_list(Bindings)->
+    Bin = term_to_binary(Query),
+    %% log:info("query: ~p~n",[Query]),
+    Port ! {self(), {command, Bin}},
+    sync(Port, Sink);
+query(Port, {SQL, Bindings}, Sink) ->
+    query(Port, {iolist_to_binary(SQL), Bindings}, Sink);
+query(Port, SQL, Sink) ->
+    query(Port, {SQL,[]}, Sink).
+
+
+sync(Port, Sink) ->
+    receive
+        {Port, {data, <<>>}} ->
+            Sink(eof);
+        {Port, {data, Bin}} ->
+            Term = binary_to_term(Bin),
+            case Term of
+                {error, Msg} -> throw({sql_error, Msg});
+                _ -> Sink({data, Term})
+            end,
+            sync(Port, Sink)
+    end.
+               
+
+
+%% Formatters
+fmt(F,A) -> tools:format(F,A).
+cs(AtomList) -> string:join([fmt("~s",[A]) || A <- AtomList],",").
+cl(all) -> "";
+cl({where, Expr}) -> fmt("where ~s",[ex(Expr)]).
+ex({'and', E1, E2}) -> fmt("~s and ~s",[ex(E1),ex(E2)]);
+ex({eq, Col, Val}) -> fmt("~s = '~s'", [atom_to_list(Col), Val]).
+     
+
+select(DB,Cols,Table,Clause,Sink) ->
+    query(DB, fmt("select ~s from ~s ~s",
+                 [cs(Cols),Table,cl(Clause)]), Sink).
+
+select(DB,Table,Clause,Sink) ->
+    query(DB, fmt("select * from ~s ~s",
+                 [Table,cl(Clause)]), Sink).
+
+select_m(DB,Cols,Table,Clause,Sink) ->
+    select(DB,Cols,Table,Clause,
+           sink:map(
+             fun(Vals) -> 
+                     maps:put(
+                       type, Table,
+                       maps:from_list(
+                         lists:zip(Cols,Vals)))
+             end, Sink)).
+
+delete(DB,Table,Clause,Sink) ->
+    query(DB, fmt("delete from ~s ~s",
+                 [Table,cl(Clause)]), Sink).
+    
+
+%% To keep the database type structure consistent, all inserts into
+%% the database need to pass through this command.
+
+%% Add an additional check to sanitize binding annotation before
+%% passing it into the query command.  Add warnings here to track down
+%% old loosely typed calls.
+typed({text, Bin}=B) when is_binary(Bin) -> B;
+typed({blob, Bin}=B) when is_binary(Bin) -> B;
+typed(Bin) when is_binary(Bin) -> {text,Bin};
+typed(Val) -> 
+    ConvVal = {text, tools:as_binary(Val)},
+    log:info("WARNING: ~p is converted to ~p before insert~n"
+            ,[Val,ConvVal]),
+    ConvVal.
+
+%% BValues is a list of {blob|text, binary()} | binary().
+%% The default binding type is text.
+insert_or_replace(DB,Cols,Table,Values,Sink) ->
+    %% log:info("insert_or_replace: args: ~p~n", [{DB,Cols,Table,Values}]),
+    [true = is_atom(A) || A <- [Table | Cols]], %% Typecheck
+    Query = {fmt("insert or replace into ~s (~s) values (~s)",
+               [Table,cs(Cols),cs(["?" || _<- Values])]),
+             [typed(V) || V <- Values]},
+    %% log:info("insert_or_replace: query: ~p~n",[Query]),
+    query(DB, Query, Sink).
+
+
+%% Insert record represent as map with type field corresponding to
+%% column name.
+insert_or_replace_m(DB,Map,Sink) ->
+    Cols = maps:keys(Map)--[type],
+    Table = maps:get(type,Map),
+    Values = [maps:get(C, Map) || C <- Cols],
+    insert_or_replace(DB,Cols,Table,Values,Sink).
+
+   
+
+
+
