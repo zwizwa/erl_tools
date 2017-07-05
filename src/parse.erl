4eb2122183663ea292c2fda41d976a3f30d98977
Tom Schouten <tom@zwizwa.be>
2017-07-05 14:56:12 -0400

    tokenize as fold

--- src/parse.erl
+++ src/parse.erl
@@ -3,40 +3,46 @@
 
 %% Parse utils
 test() ->
-    bm_tok(
-      #{ %% Used by tokenizer
-         $" => quote,
-         $\ => escape,
-         %% Left in output stream
-         ${ => open,
-         $} => close,
-         $, => comma,
-         $= => equal },
-      "download,{section=\".text\",section-sent=\"1440\",section-size=\"34032\",total-sent=\"1676\",total-size=\"625300\"").
+    Fold = 
+        bm_tok(
+          #{ %% Used by tokenizer
+             $" => quote,
+             $\ => escape,
+             %% Left in output stream
+             ${ => open,
+             $} => close,
+             $, => comma,
+             $= => equal },
+          "download,{section=\".text\",section-sent=\"1440\",section-size=\"34032\",total-sent=\"1676\",total-size=\"625300\""),
+    fold:to_list(Fold).
 
 %% Bi-modal quote/escape tokenizer with single-character controls.
-%% This structure seems quite common.
+%% This structure seems quite common for ad-hoc languages.
 bm_tok(Control, List) ->
-    bm_tok(Control,normal,[],List).
-bm_tok(_,normal,Stack,[]) -> atm(Stack);
-bm_tok(C,normal,Stack,[Char|Rest]) ->
+    fun(Fun, Init) ->
+            tok_fld(Control, normal, [], List, Fun, Init)
+    end.
+
+%% Left fold core routine.
+tok_fld(_,normal,Stack,[],F,S) -> atm(Stack,F,S);
+tok_fld(C,normal,Stack,[Char|Rest], F, S) ->
     case maps:find(Char, C) of
         {ok, escape} -> error(bad_escape);
-        {ok, quote}  -> bm_tok(C, quote, Stack, Rest);
-        {ok, Token}  -> atm(Stack) ++ [Token | bm_tok(C, normal, [], Rest)];
-        _            -> bm_tok(C, normal, [Char | Stack], Rest)
+        {ok, quote}  -> tok_fld(C, quote, Stack, Rest, F, S);
+        {ok, Token}  -> tok_fld(C, normal, [], Rest, F, F(Token,atm(Stack,F,S)));
+        _            -> tok_fld(C, normal, [Char | Stack], Rest, F, S)
     end;
-bm_tok(C,quote,Stack,[Char|Rest]) ->
+tok_fld(C,quote,Stack,[Char|Rest], F, S) ->
     case maps:find(Char, C) of
         {ok, escape} ->
             case Rest of
-                [Char1|Rest1] -> bm_tok(C,quote,[Char1|Stack],Rest1);
+                [Char1|Rest1] -> tok_fld(C,quote,[Char1|Stack],Rest1,F,S);
                 _ ->             error(bad_quote)
                     
             end;
-        {ok, quote} -> bm_tok(C,normal,Stack,Rest);
-        _ ->           bm_tok(C,quote,[Char|Stack],Rest)
+        {ok, quote} -> tok_fld(C,normal,Stack,Rest,F,S);
+        _ ->           tok_fld(C,quote,[Char|Stack],Rest,F,S)
             
     end.
-atm([]) -> [];
-atm(Stack) -> [{atom, lists:reverse(Stack)}].
+atm([], _, S) -> S;
+atm(Stack, F, S) -> F({atom,lists:reverse(Stack)},S).
