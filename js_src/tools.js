7567514f30f701d9a07100e76982e6dc68123ac8
Tom Schouten <tom@zwizwa.be>
2017-08-21 09:28:22 -0400

    tools.each delegate to forEach

--- js_src/tools.js
+++ js_src/tools.js
@@ -4,14 +4,14 @@ function each(o, f) {
     var i;
     if (o.length !== undefined) {
         // Array
-        for (i=0; i<o.length; i++) { 
-            console.log(i,o[i]);
-            f(o[i],i,o); 
-        }
+        for (i=0; i<o.length; i++) { f(o[i],i,o); }
+        return o;
+    }
+    if (o.forEach !== undefined) {
+        o.forEach(f);
         return o;
     }
     console.log("error: each", o, f);
-    throw(each,o,fun);
 }
 
 module.exports = {
