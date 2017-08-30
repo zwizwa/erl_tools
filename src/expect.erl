34833df9ea56d5bf104eb6ac88a3d26d83f221dc
Tom Schouten <tom@zwizwa.be>
2017-08-30 15:48:20 -0400

    expect, save as maps: more readable

--- src/expect.erl
+++ src/expect.erl
@@ -1,35 +1,42 @@
 -module(expect).
--export([run/1]).
-
+-export([run/2, run/3]).
 
 %% In-place operation on file.
-run(FileName) ->
-    Old = load(FileName),
-    New = update(Old),
-    save(FileName ++ ".new", New).
+run(InFile, OutFile) ->
+    run(InFile, OutFile, #{}).
 
+run(InFile, OutFile, Additional) when is_map(Additional) ->
+    Old = load(InFile),
+    New = update(maps:merge(Old, Additional)),
+    save(OutFile, New),
+    maps:from_list(
+      [{Key, maps:get(Key, New)}
+       || Key <- maps:keys(Additional)]).
 
 load(FileName) ->
     {ok, Bin} = file:read_file(FileName),
     Str = binary_to_list(Bin),
     {ok, Tokens, _} = erl_scan:string(Str),  %% Str contains trailing '.'
-    {ok, [expect|Tests]} = erl_parse:parse_term(Tokens),
-    Tests.
+    {ok, Tests = #{type := expect}} = erl_parse:parse_term(Tokens),
+    maps:remove(type, Tests).
 
 save(FileName, New) ->
-    ok = file:write_file(
-           FileName,
-           ["%% -*- erlang -*-\n",
-            "[expect\n",
-            [io_lib:format(",{~p,~n  ~p}~n",
-                           [Spec,Result]) || {Spec,Result} <- New],
-            "].\n"]).
+    IOList = 
+        ["%% -*- erlang -*-\n",
+         "#{ type => expect",
+         [io_lib:format(",~n   ~p~n   => ~p", [Spec,Result]) 
+          || {Spec,Result} <- maps:to_list(New)],
+         "\n}.\n"],
+    %% log:info("~s",[IOList]),
+    ok = file:write_file(FileName, IOList).
+    
 
 update(Tests) ->
-    [{Cmd, catch apply(Mod,Fun,Args)}
-     || {{Mod,Fun,Args}=Cmd,_} <- Tests].
+    maps:map(
+      fun({Mod,Fun,Args},_) -> catch apply(Mod,Fun,Args) end,
+      Tests).
 
-
+    
     
     
 
