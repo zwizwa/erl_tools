e6dfc1b632623a604412a6f5bfe8db748443493d
Tom Schouten <tom@zwizwa.be>
2018-03-04 17:55:40 +0100

    cleanup

--- src/hs.erl
+++ src/hs.erl
@@ -3,6 +3,7 @@
 %% 1. dump syntax to erlang term
 %% 2. parse erlang in haskel and perform embedding there
 
+
 -module(hs).
 -export([parse/1,type/1,x/1]).
 
@@ -18,44 +19,47 @@ x(Str) ->
     Top = parse(Str),
     iolist_to_binary(typedef(Top)).
 
-
-
 %% {atom(),_} is interpreted as a constructor for a new data type.
 
 %% Unions take constructor names from tags.
-typedef({attribute,Line,type,{Name,{type,_,union,Ts},Vs}}) ->
-    ["data ", type({type,Line,Name,Vs})," = ", 
-     lists:join(" | ", [alternative(T) || T <- Ts])];
+typedef({attribute,L,type,{Name,{type,_,union,Ts},Vs}}) ->
+    ["data ", type({type,L,Name,Vs})," = ", 
+     lists:join(" | ", [cons(T) || T <- Ts])];
+
 %% Special case for single-constructor types.
-typedef({attribute,Line,type,{Name,Alt={type,_,tuple,[{atom,_,_},_]},Vs}}) ->
-    ["data ", type({type,Line,Name,Vs})," = ", alternative(Alt)];
+typedef({attribute,L,type,{Name,Alt={type,_,tuple,[{atom,_,_},_]},Vs}}) ->
+    ["data ", type({type,L,Name,Vs})," = ", cons(Alt)];
 
 %% Other forms are treated as aliases.  Unions are not allowed inside type nesting.
-typedef({attribute,Line,type,{Name,T,Vs}}) ->
-    ["type ", type({type,Line,Name,Vs})," = ", type(T)];
+typedef({attribute,L,type,{Name,T,Vs}}) ->
+    ["type ", type({type,L,Name,Vs})," = ", type(T)];
 
-%% Funcion
+%% Function specs
 typedef({attribute,_,spec,{{Fname,_Nargs},[FunType]}}) ->
     [atom_to_list(Fname), " :: ", type(FunType)].
 
 
 
-     
-alternative({type,_,tuple,[{atom,_,Cons},T]}) ->
+%% Constructor syntax for sum types.     
+cons({type,_,tuple,[{atom,_,Cons},T]}) ->
     [type_name(Cons), " (", type(T), ")"].
     
 
 %% Erlang syntax structure is good enough for direct translation.
 %% FIXME: tuple
-type({type,_,'union',_}=T) -> throw({only_toplevel_union,T});
+type({type,L,'tuple',[T]}) -> type({user_type,L,s,[T]});  %% No single-element tuple in Haskell
+type({type,_,'tuple',Ts}) -> ["(", lists:join(",",[type(T) || T<-Ts]),")"];
+type({type,_,'list',[T]}) -> ["[", type(T), "]"];
 type({type,_,'fun',[A,R]}) -> [type(A)," -> ",type(R)];
-type({type,_,product,[A]}) -> type(A);  %% No single-element tuple in Haskell
-type({type,_,product,As}) -> ["(",lists:join(", ",[type(A) || A <- As]),")"];
+type({type,L,'product',As}) -> type({type,L,'tuple',As});
+type({type,_,'union',_}=T) -> throw({only_toplevel_union,T});
+type({type,_,T,[]}) -> type_name(T); %% Otherwise this is unit ().
 type({type,_,T,Ts}) -> [type_name(T), "(", lists:join(" ", [type(T0) || T0 <- Ts]), ")"];
 type({var,_,Var}) -> var_name(Var);
-type({user_type,Line,T,Vs}) -> type({type, Line, T, Vs}). %% Same
+type({user_type,L,T,Vs}) -> type({type, L, T, Vs}). %% Same
 
 %% Base type conversion.
+%%type_name(nil) -> "[]";
 type_name(Type) ->
     Bs = re:split(atom_to_list(Type),"_"),
     [to_upper_first(binary_to_list(B)) || B <- Bs].
