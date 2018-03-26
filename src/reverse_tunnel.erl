08ec94829b744260f7a6ede9b01cc02cff9e2185
Tom Schouten <tom@zwizwa.be>
2018-03-26 13:26:59 -0400

    reverse_tunnel

--- src/reverse_tunnel.erl
+++ src/reverse_tunnel.erl
@@ -1,124 +1,6 @@
 -module(reverse_tunnel).
 -export([start/2,singleshot/2,test/1]).
 
-%% Implements the equivalent of /etc/inetd.conf:
-%% # Reverse ssh tunnel.
-%% 22001 stream tcp nowait nobody /usr/bin/socat /usr/bin/socat - tcp-listen:22002,reuseaddr
-%%
-%% But using a port pool for multiple connections.
-
-info(F,A) -> log:info(F,A).
-start(_,_) ->     
-    ok.
-
-%% %% Main server
-%% start(Incoming, {LocalListen,_} = _PortPool) ->
-%%     start_simple(
-%%       Incoming,
-%%       %% Tunnel setup connection is accepted.  Spawn a
-%%       %% corresponding listener.
-%%       fun (TunnelSock) ->
-%%               Tunnel = self(),
-%%               listener(Tunnel, LocalListen),
-%%               #{ sock := TunnelSock,
-%%                  other := listener(Tunnel, LocalListen) }
-
-
-%%               info("tunnel connect: ~p~n", [TunnelSock]),
-%%               Tunnel = self(),
-%%               #{ tunnel_sock => TunnelSock,
-%%                  listener => listener(Tunnel, LocalListen) }
-%%       end,
-%%       %% All data arriving is forwarded to the listener.
-%%       fun (eof, #{ sock := Sock}=State) ->
-%%               _ = gen_tcp:close(Sock), State;
-%%           ({data, Data}, #{ sock := _Sock,
-%%                             listener := _Listener } = State) ->
-%%               log:info("tunnel incoming: ~p~n",[Data]),
-%%               State;
-%%           ({forward, Data}, #{ sock := Sock } = State) ->
-%%               gen_tcp:send(Sock, Data),
-%%               State;
-%%       end).
-
-
-%% %% Message handlers are the same on both the tunnel socket and the
-%% %% local socket.
-%% forward_handler(_Tag) ->
-%%     fun(eof, #{ sock := Sock} = State) ->
-%%             _ = gen_tcp:close(Sock),
-%%             State;
-%%        ({data, Data}=_M, #{ other := Other } = State) ->
-%%             info("~p: ~p~n", [{_Tag,_M}]),
-%%             Other ! {send, Data},
-%%             State;
-%%        ({send, Data}, #{ sock := Sock } = State) ->
-%%             info("~p: ~p~n", [{_Tag,_M}]),
-%%             gen_tcp:send(Sock, Data),
-%%             State;
-%%        end.
-
-
-%% %% Per tunnel listener
-%% listener(Tunn, LocalListen) ->
-%%     start_simple(
-%%       LocalListen,
-%%       %% Local connection is accepted.
-%%       fun (LocalSock) ->
-%%               info("local connect: ~p~n", [LocalSock]),
-%%               #{ local_sock => LocalSock }
-%%       end,
-%%       %% All data arriving is forwarded to the listener.
-%%       fun (eof, #{ sock := Sock}=State) ->
-%%               _ = gen_tcp:close(Sock), State;
-%%           ({data,Data}, State) ->
-              
-%%               log:info("local incoming: ~p~n",[Data]),
-%%               gen_tcp:send(TunnelSock, Data),
-%%               State
-%%       end).
-
-%% %% API is a little weird.  We only need this basic template.
-%% start_simple(Port,Init,Handle) ->
-%%     serv:start(
-%%     {handler,
-%%      fun() ->
-%%              serv_tcp:init(
-%%                [Port],
-%%                {handler,
-%%                 %% Initialize after accept
-%%                 fun(Sock,_Port) -> Init(Sock) end,
-%%                 %% Handle incoming data
-%%                 Handle},
-%%                %% Handle other incoming messages
-%%                fun obj:handle/2,
-%%                %% State for messages
-%%                #{})
-%%      end,
-%%      %% Use default handler
-%%      fun serv_tcp:handle/2}).
-                
-                        
-             
-               
-
-%% %% This is hard to express without an abstraction: a single-shot
-%% %% server.
-
-%% singleshot(Port, Connect) ->
-%%     start_simple(
-%%       Port,
-%%       fun(
-%%       Connect,
-      
-      
-%%       fun(Socket) ->
-              
-
-                                   
-
-
-
 %% The point is to:
 %% - Provide the machinery to produce 2 sockets
 %% - Set them up so they forward to each other
@@ -126,6 +8,73 @@ start(_,_) ->
 
 %% The key element here is a single shot connection, which is easier
 %% to write a custom routine for as opposed to useing serv_tcp.
+
+%% Implements the equivalent of /etc/inetd.conf:
+%% # Reverse ssh tunnel.
+%% 22001 stream tcp nowait nobody /usr/bin/socat /usr/bin/socat - tcp-listen:22002,reuseaddr
+%%
+%% But using a port pool for multiple connections.
+
+info(F,A) -> log:info(F,A).
+
+%% Main server
+start(Incoming, {LocalListen,_} = _PortPool) ->
+
+    serv:start(
+      {handler,
+       fun() ->
+               Opts = [binary, {packet, 0}, {active, true}, {reuseaddr, true}],
+               serv_tcp:init(
+                 [Incoming],
+                 {handler,
+                  %% Initialize after accept
+                  fun(TunnelSock, _Port) -> 
+                          %% Once an incoming tunnel is set up, we set
+                          %% up a corresponding single-shot listening
+                          %% socket.
+                          Tunnel = self(),
+                          singleshot(
+                            LocalListen,
+                            {handler,
+                             fun(LocalSock) ->
+                                     %% Once that is set up, both are connected.
+                                     Tunnel ! {set_other, self()},
+                                     #{ sock => LocalSock,
+                                        other => Tunnel }
+                             end,
+                             fun forward_handle/2}),
+                          #{ sock => TunnelSock,
+                             other => not_yet_connected }
+                  end,
+                  fun forward_handle/2},
+                 %% Handle other incoming messages: FIXME
+                 fun obj:handle/2,
+                 %% State for messages
+                 #{},
+                 Opts)
+       end,
+       %% Use default handler
+       fun serv_tcp:handle/2}).
+
+%% Forwarder, same for both ends.
+forward_handle(Msg,State) ->
+    log:info("~p~n", [Msg]),
+    fw_handle(Msg, State).
+fw_handle({tcp, _, Data}=_M, #{ other := Other }=State) ->
+    Other ! {send, Data},
+    State;
+fw_handle({send, Data}, #{ sock := Sock}=State) ->
+    gen_tcp:send(Sock, Data),
+    State;
+fw_handle({set_other, Other}, State) ->
+    maps:put(other, Other, State).
+
+    
+
+    
+
+
+
                
 singleshot(ListenPort, {handler, Init, Handle}) ->
     Opts = [binary, {packet, 0}, {active, true}, {reuseaddr, true}],
@@ -135,34 +84,33 @@ singleshot(ListenPort, {handler, Init, Handle}) ->
                Listener = self(),
                case gen_tcp:listen(ListenPort, Opts) of
                    {error, Reason} ->
-                       info("listen error: port ~p: ~p~n", [ListenPort,Reason]),
+                       info("singleshot listen error: port ~p: ~p~n", [ListenPort,Reason]),
                        exit(Reason);
                    {ok, ListenSock} ->
-                       info("listening: ~p~n", [ListenPort]),
+                       info("singleshot listening: ~p~n", [ListenPort]),
                        serv:start(
                          {handler,
                           fun() ->
                                   case gen_tcp:accept(ListenSock) of
                                       {error, Reason}  ->
-                                          info("error: ~p~n", [{ListenPort,Reason}]),
+                                          info("singleshot accept error: ~p~n", [{ListenPort,Reason}]),
                                           exit(Reason);
                                       {ok, ConnSock} -> 
-                                          Listener ! accepted,
-                                          gen_tcp:send(ConnSock, "Hello\n"),
-                                          info("accepted: ~p~n", [{ListenPort,ConnSock}]),
+                                          Listener ! close,
+                                          info("singleshot accepted: ~p~n", [{ListenPort,ConnSock}]),
                                           Init(ConnSock)
-                                          
                                   end
                           end,
                           Handle}),
-                       receive
-                           Msg -> info("listener: ~p~n",[Msg])
-                       end,
-                       info("closing: ~p~n", [ListenPort]),
+                       %% Need to wait until socket is accepted
+                       receive close -> ok end,
+                       info("singleshot closing listener: ~p~n", [ListenPort]),
                        gen_tcp:close(ListenSock),
                        ok
                end
        end}).
+
+
 
 
 
