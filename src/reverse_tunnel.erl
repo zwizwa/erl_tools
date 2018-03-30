20eb16d8bce59e3fc2a85e9532f660cbb389fbbb
Tom Schouten <tom@zwizwa.be>
2018-03-30 09:02:27 -0400

    cleanup reverse_tunnel

--- src/reverse_tunnel.erl
+++ src/reverse_tunnel.erl
@@ -3,36 +3,42 @@
 
 %% Create a reverse TCP tunnel.
 
+%% This was originally written to be able to forward SSH connections
+%% without an ssh client on the target.  If you have an ssh client and
+%% key infrastructure setup, it is probably better to use that
+%% instead.
 
-%% Implements the equivalent of
+%% The server implemented here  on the other end of a client:
+%% socat tcp-connect:localhost:22 tcp-connect:$SERVER_IP:22001
 %%
-%% SERVER: /etc/inetd.conf:
-%% # Reverse ssh tunnel.
+%% Note the 2 'tcp-connect' ends.  At the server end we need to
+%% connect two listening sockets.
+%%
+%% A way to do that with socat is to put in /etc/inetd.conf:
 %% 22001 stream tcp nowait nobody /usr/bin/socat /usr/bin/socat - tcp-listen:22002,reuseaddr
 %%
-%% CLIENT: socat tcp-connect:localhost:22 tcp-connect:$SERVER_IP:22001
+%% This way, when a client connects to 22001, a new listeneing socket
+%% is created on 22002, which can then be used to log into the
+%% client's forwarded port 22.
 %%
-%% The exposed client port is then available on the host:
-%% ssh -p 22002 localhost
+%% The socat solution only allows one connection.  The code below is a
+%% generalization to multiple connections.
 
-%% We do something similar, but support multple connections.
 
-%% The point is to:
-%% - Provide the machinery to produce 2 sockets
-%% - Set them up so they forward to each other
-%% - For the local listener, to accept only one connection
 
-%% The key element here is a single shot connection, which is easier
-%% to write a custom routine for as opposed to useing serv_tcp.
-
-%% This code was surprisingly hard to get right.
+%% A key element in the implementation is to provide the mechanism of
+%% a single-shot listening socket.  There are some subtleties in
+%% getting that to work right.
+%%
+%% For the main incoming server the client's socat will connect to we
+%% just use a serv_tcp instance.
 
 
 info(F,A) -> log:info(F,A).
 
 -define(OPTS,[binary, {packet, 0}, {active, true}, {reuseaddr, true}]).
 
-%% Main server
+%% Main TCP server.  Clients will connect here to set up reverse tunnel.
 start(Incoming, PortRange) ->
     serv:start(
       {handler,
@@ -41,33 +47,7 @@ start(Incoming, PortRange) ->
                serv_tcp:init(
                  [Incoming],
                  {handler,
-                  %% Initialize after accept
-                  fun(TunnelSock, _Port) -> 
-                          %% Once an incoming tunnel is set up, we set
-                          %% up a corresponding single-shot listening
-                          %% socket.
-                          Tunnel = self(),
-                          %% For info/1
-                          HavePort = fun(ListenPort) -> obj:set(Tunnel, listen_port, ListenPort) end,
-                          OnError = fun() -> Tunnel ! close end,
-                          SingleShot = 
-                              singleshot(
-                                PortRange, HavePort, OnError,
-                                {handler,
-                                 fun(LocalSock) ->
-                                         link(Registry),
-                                         %% Once that is set up, both are connected.
-                                         Tunnel ! {set_connected, self()},
-                                         #{ sock => LocalSock,
-                                            buffer => [],
-                                            other => {connected, Tunnel} }
-                                 end,
-                                 fun reverse_tunnel:forward_handle/2}),
-
-                          #{ sock => TunnelSock,
-                             buffer => [],
-                             other => {waiting, SingleShot} }
-                  end,
+                  fun(TunnelSock, _) -> forward_init(PortRange, TunnelSock, Registry) end,
                   fun reverse_tunnel:forward_handle/2},
                  %% Handle other incoming messages
                  fun ({'EXIT',_Pid,_Reason}=Msg, State) ->
@@ -86,8 +66,52 @@ start(Incoming, PortRange) ->
 
 
 
-%% Forwarder, same code for both ends.
+forward_init(PortRange, TunnelSock, Registry) ->
+    %% TunnelSock is an accepted incoming connection.  Create a new
+    %% single-shot listening socket.
+    Tunnel = self(),
+    SingleShot = 
+        singleshot(
+
+          %% The code that sets up the corresponding listener picks a
+          %% port from the port range that is not busy.
+          PortRange,
+
+          %% Once the listening port is opened, this gets called so we
+          %% can save the port number for later use by info/1.
+          fun(ListenPort) -> obj:set(Tunnel, state, {port, ListenPort}) end,
+
+          %% When anything goes wrong, we close up.
+          fun() -> Tunnel ! close end,
+
+          %% Once the local listening port is connected to, the
+          %% processes associated to the two sockets are associated
+          %% such that they can forward TCP traffic between them.
+          {handler,
+           fun(LocalSock) ->
+                   link(Registry),
+                   Tunnel ! {set_connected, self()},
+                   obj:set(Tunnel, state, {connected, {Tunnel, self()}}),
+                   #{ sock => LocalSock,
+                      buffer => [],
+                      other => {connected, Tunnel} }
+           end,
+           fun reverse_tunnel:forward_handle/2}),
+    
+    #{ sock => TunnelSock,
+       buffer => [],
+       other => {waiting, SingleShot}
+     }.
+
+
+%% A tunnel conists of two processes, one for each end.  Each of those
+%% processes is associated to an accepted incoming socket: one comes
+%% from the main incoming TCP. server, the other is from the local
+%% one-shot TCP server.  Once the two processes are associated,
+%% behavior is symmetric, so they use the same message handling method.
+
 forward_handle(Msg, State) ->
+    %% Debugging tap
     %% log:info("~p~n", [Msg]),
     fw_handle(Msg, State).
 
@@ -102,7 +126,7 @@ fw_handle({tcp_closed,_}, #{ other := {waiting, SingleShot} }) ->
     SingleShot ! close,
     exit(self(), normal);
 
-fw_handle({tcp_closed,_}, #{ other := {connected, Other} }) when is_pid(Other) ->
+fw_handle({tcp_closed,_}, #{ other := {connected, Other} }) ->
     Other ! close,
     exit(self(), normal);
 
@@ -111,7 +135,7 @@ fw_handle(close, #{ sock := Sock}) ->
     exit(self(), normal);
 
 fw_handle({set_connected, Other}, State) ->
-    %% Once other end is there, dump the buffer (e.g. SSH banner).
+    %% Once other end is there, flush dump the buffer (e.g. contains SSH banner).
     State1 = maps:put(other, {connected, Other}, State),
     flush(State1);
 
@@ -137,7 +161,8 @@ flush(#{ other := {connected, Other}, buffer := Buffer} = State) ->
 
     
 
-%% Try Fun given a range.
+%% Try Fun until {ok,_}, given an integer range.  This is used
+%% together with gen_tcp:listen/2 to find an available listening port.
 until_ok(Fun, {Start, Endx}) ->
     case Start >= Endx of
         true ->
@@ -150,7 +175,8 @@ until_ok(Fun, {Start, Endx}) ->
                     until_ok(Fun, {Start+1,Endx})
             end
     end.
-               
+
+%% Listen, then stop listening once a connection is established.               
 singleshot(PortRange,
            HavePort,
            OnError,
@@ -202,9 +228,9 @@ info(Server) ->
     receive
         {Ref, Pids} ->
             [begin
-                 {obj:get(P, listen_port),        %% Where we need to connect
+                 {obj:get(P, state),              %% {listening,Port} or {connected,_}
                   inet:peername(obj:get(P, sock)),%% Incoming tunnel connection
-                  obj:get(P, buffer)}             %% Banner
+                  obj:get(P, buffer)}             %% Forwarded server's TCP banner
              end || P <- Pids]
     end.
 
