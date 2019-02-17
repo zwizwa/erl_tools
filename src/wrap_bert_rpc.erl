fdb2d22409416b1ba0b1176df0a8b512746ee9f3
Tom Schouten <tom@zwizwa.be>
2019-02-17 12:42:28 -0500

    bert

--- src/wrap_bert_rpc.erl
+++ src/wrap_bert_rpc.erl
@@ -1,18 +1,20 @@
--module(connect_bert_rpc).
+-module(wrap_bert_rpc).
 -export([start_link/1, handle/2]).
 
 %% Wrapper / monitor for BERT RPC servers.
 
-start_link(#{ spec := {_Host, _Port},
-              start := _Start} = Info) ->
+start_link(#{ spec := {_Host, _Port}} = Info) ->
     {ok, 
      serv:start(
        {handler,
         fun() -> self() ! connect, Info end,
-        fun connect_bert_rpc:handle/2})}.
+        fun wrap_bert_rpc:handle/2})}.
 
-handle(connect, #{ spec := Spec, start := Start } = State) ->
-    maps:put(sock, connect(Spec,Start,3), State);
+handle(connect, State) ->
+    Retries = maps:get(retries, State, 1),
+    State1 = maps:put(sock, connect(State, Retries), State),
+    OnConnect = maps:get(on_connect, State1, fun(S) -> S end),
+    OnConnect(State1);
 
 handle({Pid, {M,F,A}}, #{ sock := Sock } = State) ->
     gen_tcp:send(Sock, term_to_binary({call,M,F,A})),
@@ -26,28 +28,27 @@ handle({Pid, {M,F,A}}, #{ sock := Sock } = State) ->
     State;
 
 handle({tcp_closed,Sock}=_Msg, #{ sock := Sock }=State) ->
-    log:inco("~p~n", [_Msg]),
+    log:info("~p~n", [_Msg]),
     handle(connect, State);
                     
 handle(Msg,State) ->
     obj:handle(Msg,State).
 
-connect(Spec,_,0) ->
-    exit({error_connect, Spec});
-connect({Host,Port}=Spec,Start,Tries) ->
+connect(State,0) ->
+    exit({error_connect, State});
+connect(State=#{spec := {Host,Port}}, Tries) ->
     case gen_tcp:connect(
            Host,Port,
            [{active,true},{packet,4},binary]) of
         {ok, Sock} ->
-            log:info("connected: ~p~n", [Spec]),
+            log:info("connected: ~p~n", [{Host,Port}]),
             Sock;
         Error ->
             log:info("error: ~p~n", [Error]),
+            Start = maps:get(start, State, fun() -> ok end),
             Start(),
-            Ms = 1000,
-            log:info("waiting ~p ms to reconnect~n", [Ms]),
+            Ms = maps:get(delay, State, 1000),
+            %% blog:info("waiting ~p ms to reconnect~n", [Ms]),
             timer:sleep(Ms),
-            connect(Spec, Start, Tries-1)
+            connect(State, Tries-1)
     end.
-            
-         
