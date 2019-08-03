fc1a8d13d117277e10f3151f7a231c0d2f611045
Tom Schouten <tom@zwizwa.be>
2019-08-03 12:25:30 -0400

    db_html.erl

--- src/db_html.erl
+++ src/db_html.erl
@@ -1,1 +1,29 @@
-../erl/apps/ui/src/db_html.erl
+-module(db_html).
+-export([table/2, sql/2, sql/3, q/2]).
+
+table(DB, Table) ->
+    {table,[],
+     [{tr,[],
+       [{td,[],[[Col]]}
+        || Col <- Row]}
+      || Row <- q(DB, {table,Table})]}.
+
+
+    
+
+sql(DB,Sql) ->
+    sql(DB,Sql,[]).
+sql(DB,Sql,Args) when is_binary(Sql) and is_list(Args) -> 
+    try hd(sqlite3:sql(DB, [{Sql,Args}]))
+    catch C:E -> throw({C,E,Sql,Args}) end;
+sql(DB,Sql,Args) ->
+    sql(DB,iolist_to_binary(Sql),Args).
+            
+%% Some misc high level queries.
+
+q(DB, tables) ->
+    sql(DB, "SELECT name FROM sqlite_master WHERE type='table'");
+q(DB, views) ->
+    sql(DB, "SELECT name FROM sqlite_master WHERE type='view'");
+q(DB, {table, Table}) when is_atom(Table)  ->
+    sql(DB, ["SELECT * from ", atom_to_list(Table)]).
