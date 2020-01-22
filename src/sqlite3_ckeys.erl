d5d28bfa81da7f47695b4b011b25c243ee141666
Tom Schouten <tom@zwizwa.be>
2020-01-21 21:56:38 -0500

    sqlite3_ckeys cleanup

--- src/sqlite3_ckeys.erl
+++ src/sqlite3_ckeys.erl
@@ -1,19 +1,22 @@
 -module(sqlite3_ckeys).
--export([columns/2, key_info/2, schema/2, create/3, find/3, test/1]).
+-export([create/3, find/3, put/4, test/1]).
 
-%% Bridge composite keyed find/put with a relational model.  The need
-%% for this arose in exo_net, which has two sides:
+%% Bridging relational and functional model seems simplest to do by
+%% using composite keys and composite values.  We agument that with a
+%% schema for Erlang<->String conversion.
+
+%% The need for this arose in exo_net, which has two sides:
 %%
-%% - Get/Set update to implement highly available eventually
-%%   consistent distributed key-value store
+%% - Get/Set update to implement available, eventually consistent
+%%   distributed key-value store
 %%
 %% - Read-only queries on the local relational database.
 %%
 %%
 %% To implement the former, we use composite key structure,
-%% e.g. {Table,K1,K2}, where the first key refers to the table name,
+%% e.g. [Table,K1,K2], where the first key refers to the table name,
 %% and subsequent keys refer to columns in the table.  Positional
-%% encoding is used to make updates simple.
+%% encoding is used.
 %%
 %% To allow for more elaborate queries, use a fully relational model
 %% where each of the tables has names that are optimized to perform
@@ -23,30 +26,8 @@
 %%
 %% This code seems general enough to fork off into a module.
 %%
-%% Note: this does not perform Erlang type conversion.  That can be
-%% built on top.  Also: FIXME: remove tuple interface.  Lists are more
-%% convenient to later put type conversion on top.
-
-%% Keep this simple.  Table is the SQLite table name and is required
-%% to be a symbol.  The other keys just need to be serializable as
-%% pterm.
-
-%% Given a key, return all the schema information necessary to perform
-%% a get/set operation: table, key/value pairs for query, and column
-%% names for composite value.
-
-key_info(Schema, {list, [Table|Keys]}) when is_atom(Table) ->
-    {KeyNames, ValNames} = Schema(Table),
-    KeyNVs = [{maps:get(N,KeyNames),encode(K)} || {N,K} <- pos_key_list(KeyNames)],
-    ValNs = [ValName || {_, ValName} <- pos_key_list(ValNames)],
-    {Table, lists:zip(KeyNVs,Keys), ValNs};
-key_info(Schema, Key) when is_tuple(Key) ->
-    key_info(Schema, {list, tuple_to_list(Key)}).
 
 
-columns(Schema, Table) when is_atom(Table) ->
-    {Keys,Vals} = Schema(Table),
-    key_list(Keys) ++ key_list(Vals).
 
 commas(Syms) ->
      tl(lists:append(
@@ -55,24 +36,21 @@ wheres(Syms) ->
      tl(lists:append(
           [[" and ", atom_to_list(Col), "=?"] || Col <- Syms])).
 
-schema(Schema,Table) ->    
-    [$(,commas(columns(Schema,Table)), $)].
+sql(DB,Qs) ->
+    sqlite3:sql(DB,Qs).
 
-sql(DB,Sql,Args) ->
-    [QRes] = sqlite3:sql(DB, [{Sql,Args}]),
-    QRes.
-sql(DB,Sql) ->
-    sql(DB,Sql,[]).
-
+columns(Schema, Table) when is_atom(Table) ->
+    {Keys,Vals} = Schema(Table),
+    key_list(Keys) ++ key_list(Vals).
 
 %% CREATE/FIND/PUT QUERIES.  All other queries can be implemented by
 %% ad-hoc SQL queries against the relational schema.
 create(DB, Schema, Table) ->
     SQL = tools:format_binary(
-            "create table if not exists '~p' ~s",
-            [Table, schema(Schema, Table)]),
+            "create table if not exists '~p' (~s)",
+            [Table, commas(columns(Schema, Table))]),
     log:info("~s~n", [SQL]),
-    sql(DB, SQL),
+    sql(DB, [{SQL,[]}]),
     ok.
 
 find_q(Schema, Table) ->
@@ -81,22 +59,17 @@ find_q(Schema, Table) ->
             "select ~s from '~p' where ~s",
             [commas(key_list(ValNames)), Table,
              wheres(key_list(KeyNames))]),
-    Sql.
-find(DB, Schema, {table_and_key_list, [Table | Keys]}) ->
-    Sql = find_q(Schema, Table),
+    Types = type_list(ValNames),
+    {Sql, Types}.
+find(DB, Schema, [Table | Keys]) ->
+    {Sql,Types} = find_q(Schema, Table),
     Args = [encode(K) || K <- Keys],
-    log:info("find: ~999p~n",[{Sql,Args}]),
-    sql(DB,Sql,Args);
-        
-find(DB, Schema, Key) when is_tuple(Key) ->
-    find(DB, Schema, {table_and_key_list, tuple_to_list(Key)}).
+    %% log:info("find: ~999p~n",[{Sql,Args}]),
+    case sql(DB,[{Sql,Args}]) of
+        [[]] -> error;
+        [[Vals]] -> {ok, [type:decode(TV) || TV <- lists:zip(Types,Vals)]}
+    end.
 
-
-%%    sql(DB, <<"insert or replace into 'exo_net' (var,val,ts,type) values (?,?,?,?)">>, 
-%%        [encode(Var), BinVal, encode(Ts), encode(Type)]).
-
-%% FIXME: This will need primary key, or replace won't work.  Maybe do
-%% drop then insert transaction?
 put_q(Schema, Table) ->
     {KeyNames, ValNames} = Schema(Table),
     Sql1 = tools:format_binary(
@@ -106,44 +79,37 @@ put_q(Schema, Table) ->
     Sql2 = tools:format_binary(
              "insert into '~p' (~s) values (~s)",
              [Table, commas(Cols), commas(['?' || _ <- Cols])]),
-    {Sql1,Sql2}.
-put(DB, Schema, {table_and_key_list, [Table | Keys]}, {values, Vals}) ->
-    {Sql1,Sql2} = put_q(Schema, Table),
+    Types = type_list(ValNames),
+    {Sql1,Sql2,Types}.
+put(DB, Schema, [Table | Keys], Vals) ->
+    {Sql1,Sql2,Types} = put_q(Schema, Table),
     BKeys = [encode(K) || K <- Keys],
-    %% Note: Vals are supposed to be binary.  Encoding is not done in this layer.
-    log:info("put: ~999p~n",[{Sql1,BKeys}]),
-    log:info("put: ~999p~n",[{Sql2,BKeys++Vals}]),
-    [] = sql(DB,Sql1,BKeys),
-    [] = sql(DB,Sql2,BKeys++Vals),
-    ok;
-put(DB, Schema, Key, Val) when is_tuple(Key) and is_tuple(Val)->
-    put(DB, Schema,
-        {table_and_key_list, tuple_to_list(Key)},
-        {values, tuple_to_list(Val)});
-put(DB, Schema, Key, Val) when is_tuple(Key) ->
-    put(DB, Schema, Key, {Val}).
+    BVals = [type:encode(TV) || TV <- lists:zip(Types,Vals)],
+    Q1 = {Sql1,BKeys},
+    Q2 = {Sql2,BKeys++BVals},
+    %% log:info("put: ~999p~n",[Q1]),
+    %% log:info("put: ~999p~n",[Q2]),
+    [[],[]] = sql(DB, [Q1,Q2]),
+    ok.
 
-
-%% find(DB,Key) ->
-%%     SQL = tools:format_binary(
-%%             "select ~s from '~p' where 
-%%     case 
-%%         sql(DB, <<"select val,ts,type from 'exo_net' where var = ?">>, 
-%%             [encode(Var)])
-%%     of
-%%         [[]] ->
-%%             {error,{not_found, Var}};
-%%         [[[BinVal,BinTs,BinType]]] ->
-%%             Val = type:decode({decode(BinType),BinVal}),
-%%             {ok, {decode(BinTs),Val}}
-%%     end.
 
 
 
 %% TOOLS
 
+%% Default encoding is pterm, but this isn't convenient for everything
+%% (e.g. mac addresses).
+spec(Key) when is_atom(Key) -> {pterm, Key};
+spec({Type,Key}=Spec) when is_atom(Type) and is_atom(Key) -> Spec.
+key(Spec)  -> {_,Key}  = spec(Spec), Key.
+type(Spec) -> {Type,_} = spec(Spec), Type.
+
+    
+
 key_list(PosToNameMap) ->
-    [Key || {_, Key} <- pos_key_list(PosToNameMap)].
+    [key(TypeAndKey) || {_, TypeAndKey} <- pos_key_list(PosToNameMap)].
+type_list(PosToNameMap) ->
+    [type(TypeAndKey) || {_, TypeAndKey} <- pos_key_list(PosToNameMap)].
 pos_key_list(PosToNameMap) ->
     lists:sort(maps:to_list(PosToNameMap)).
 encode(Term) ->
