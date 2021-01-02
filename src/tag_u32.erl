e7d3ca9171d45f4ea1920de4ea7e00c16a389c8c
Tom Schouten <tom@zwizwa.be>
2021-01-02 14:06:22 -0500

    tag_u32:dict/1 + command refactor

--- src/tag_u32.erl
+++ src/tag_u32.erl
@@ -1,31 +1,55 @@
 %% Protocol wrappers for TAG_U32-based RPC protocol.
 
 -module(tag_u32).
--export([call/2, ctrl/2, nb_atoms/1, name/2, dict/1]).
+-export([call/2, meta/3, name/2, type/2, dict/1]).
 
 %% Generic RPC
+%%
+%% In this module we use reverse path notation, bottom-to-tom,
+%% i.e. zipper or stack view, which is easier to work with.  TAG_U32
+%% uses top-to-bottom order.
 call(Pid, Sub) ->
-    obj:call(Pid, {req_u32, Sub}, 2000).
-%% Generic control path
-ctrl(Pid, Sub) ->
-    call(Pid, [-1 | Sub]).
-%% Get number of atoms in dictionary.
-nb_atoms(Pid) ->
-    {[NbEl], _} = ctrl(Pid, [0]), NbEl.
-%% Map index in dictionary list to identifier (this is likely the identity function)
-atom_index_to_id(Pid, Idx) ->
-    {[Id], _} = ctrl(Pid, [1, Idx]), Id.
-%% Map identifier to name string.
-name(Pid, Id) ->
-    %% log:info("name ~p~n",[Id]),
-    {[], Name} = ctrl(Pid, [2,Id]),
+    obj:call(Pid, {req_u32, lists:reverse(Sub)}, 2000).
+
+%% Metatdata commands
+meta(Pid, [N|Path0], Cmd) ->
+    {[], Name} = call(Pid, [N, Cmd, 16#FFFFFFFF | Path0]),
     binary_to_atom(Name,utf8).
+
+%% Map identifier to name string.
+name(Pid, Path) -> meta(Pid, Path, 2).
+type(Pid, Path) -> meta(Pid, Path, 3).
+
 %% Retreive the whole dictionary.
-dict(Pid) ->
-    fold:to_list(
-      fold:map(
-        fun(Idx) ->
-                Id = atom_index_to_id(Pid, Idx),
-                {Id, name(Pid, Id)} end,
-        fold:range(nb_atoms(Pid)))).
+
+%% Old style.  Currently just using name/2.
+%% dict(Pid) ->
+%%     fold:to_list(
+%%       fold:map(
+%%         fun(Idx) ->
+%%                 Id = atom_index_to_id(Pid, Idx),
+%%                 {Id, name(Pid, Id)} end,
+%%         fold:range(nb_atoms(Pid)))).
     
+dict(Pid) ->
+    sub(Pid, []).
+sub(Pid, Path) ->
+    maps:from_list(dict_list(Pid, 0, Path))
+dict_list(Pid,N,Path) ->exo
+    case name(Pid,[N|Path]) of
+        '' -> [];
+        Name -> 
+            Path1 = [N|Path],
+            Type = type(Pid,Path1),
+            Sub =
+                case Type of
+                    map ->
+                        %% Recurse.
+                        sub(Pid, Path1);
+                    _ ->
+                        %% Atoms don't have substructure, just a type.
+                        Type
+                end,
+            [{Name,{N,Sub}} | dict_list(Pid, N+1, Path)]
+    end.
+            
