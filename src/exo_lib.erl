a9a117dd7e7233e53688cede74b9ab65273d2810
Tom Schouten <tom@zwizwa.be>
2022-08-27 08:54:22 -0400

    exo_lib

--- src/exo_lib.erl
+++ src/exo_lib.erl
@@ -1,6 +1,6 @@
-%% Exo refers to exocortex: an external information processing system
-%% that augments the brain's biological high-level cognitive
-%% processes.
+%% The name "exo" refers to exocortex: an external information
+%% processing system that augments the brain's biological high-level
+%% cognitive processes.
 %%
 %% I take that to mean "network operating system".
 %%
@@ -8,15 +8,13 @@
 %% 'exo_sup'.  This module 'exo_lib' exposes functionality that can be
 %% used to implement a (private) 'exo' Erlang app.  (FIXME: Example)
 %%
-%% Required functionality in 'exo'  (FIXME: Erlang module spec).
-%%
-%% child_mfa/1   %% Maps task ID to {Module,Function,Args) start method
 
 
 -module(exo_lib).
--export([start/1, stop/1,
-         start_child/1,
-         child_spec/1
+-export([start/1, stop/1, need/1, need_hubs/1,
+         start_child/1, child_spec/1, children/0,
+         pids/1, pid/2, pid/1, pid_maybe/2, pid_maybe/1
+         
         ]).
 
 start(Id) ->
@@ -26,6 +24,19 @@ start(Id) ->
 stop(Id) ->
     supervisor:terminate_child(exo_sup, Id),
     supervisor:delete_child(exo_sup, Id).
+
+need(Id) ->
+    case exo:start(Id) of
+        {error,{already_started,Pid}} -> Pid;
+        {ok, Pid} -> Pid
+    end.
+need_hubs(Name) ->
+    case pids(Name) of
+        [] -> [need(Name)];
+        Pids -> Pids
+    end.
+
+
 
 %% Supervisor child specification for each task.
 child_spec(Id) ->
@@ -47,3 +58,78 @@ start_child(Id) ->
     end,
     {ok, Pid}.
 
+children() ->
+    maps:from_list(
+      [{Id,Pid} || {Id,Pid,_,_} <- supervisor:which_children(exo_sup)]).
+
+exo_nodes() ->
+    [node()|nodes()].
+
+
+pid(ExoName, Node) ->
+    case pids_(ExoName, Node) of
+        [Pid] -> Pid;
+        [] -> throw({no_pid, ExoName, Node})
+    end.
+pid(ExoName) ->
+    %% Always try local first
+    case pids_(ExoName, 
+               fun children/0, 
+               fun() -> whereis(ExoName) end) of
+        [Pid] -> Pid;
+        _ ->
+            case pid_maybe(ExoName, nodes()) of
+                {ok, Pid} -> Pid;
+                error -> throw({no_pid,ExoName})
+            end
+    end.
+
+pids(ExoName) ->
+    lists:flatten(
+      maps:values(
+        tools:pmap(
+          fun(Node) -> pids_(ExoName, Node) end,
+          exo_nodes()))).
+pids_(ExoName, Node) ->
+    ChildMap = fun() -> rpc_call(Node,exo,children,[]) end,
+    Whereis  = fun() -> rpc_call(Node,erlang,whereis,[ExoName]) end,
+    pids_(ExoName, ChildMap, Whereis).
+pids_(ExoName, ChildMap, Whereis) ->
+    case maps:find(ExoName, ChildMap()) of
+        {ok, Pid} -> [Pid];
+        _ when is_atom(ExoName) ->
+            %% special case for now.  FIXME: don't rely
+            %% on registered processes.
+            case Whereis() of
+                Pid when is_pid(Pid) -> [Pid];
+                _ -> []
+            end;
+        _ -> []
+    end.
+
+%% Find a representative.  Note that this no longer cares about
+%% duplicates: it just picks the first one it finds.  pids/1 is too
+%% slow because it queries every node.
+pid_maybe(ExoName, Nodes) ->
+    tools:race(
+      Nodes,
+      2000,
+      fun(Node) ->
+              case pids_(ExoName, Node) of
+             [Pid|_] -> {ok, Pid};
+             _ -> error
+         end
+      end).
+pid_maybe(ExoName) ->    
+    try {ok, pid(ExoName)}
+    catch {no_pid,_} -> error end.
+
+
+
+rpc_call(Node,M,F,A) ->
+    case rpc:call(Node,M,F,A) of
+        {badrpc,_}=Err ->
+            throw(Err);
+        Rv ->
+            Rv
+    end.
